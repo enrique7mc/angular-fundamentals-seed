68ee3ded1a27e3a8f8cd1a4f8fbcf0eb7cf4593c
Enrique Munguía <enrique7mc@gmail.com>
2017-02-01 13:31:02 -0600

    create stateless components

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,22 +7,13 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['passenger-dashboard.component.scss'],
   template: `
     <div>
-    <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span
-            class="status"
-            [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullName }}
-          <div class="date">
-            Checked in date: 
-            {{ passenger.checkedIn ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
-          </div>
-          <div class="children">
-            Children: {{ passenger.children?.length || 0 }}
-          </div>
-        </li>
-      </ul>
+      <passenger-count
+        [items]="passengers">
+      </passenger-count>
+      <passenger-detail
+        *ngFor="let passenger of passengers;"
+        [detail]="passenger">
+      </passenger-detail> 
     </div>
   `
 })
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 
+// Components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+
 @NgModule({
   declarations: [
-    PassengerDashboardComponent
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
   ],
   imports: [
     CommonModule
