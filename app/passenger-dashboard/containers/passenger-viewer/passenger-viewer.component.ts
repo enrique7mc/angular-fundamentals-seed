722da0d624db61fc26f7b69d51aa2b8b9cbff949
Enrique Munguía <enrique7mc@gmail.com>
2017-02-19 19:36:46 -0800

    add ngSubmit

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -9,7 +9,10 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['passenger-viewer.component.scss'],
   template: `
     <div>
-      <passenger-form [detail]="passenger"></passenger-form>
+      <passenger-form
+        [detail]="passenger"
+        (update)="onUpdatePassenger($event)">
+      </passenger-form>
     </div>
   `
 })
@@ -24,5 +27,13 @@ export class PassengerViewerComponent implements OnInit {
       .subscribe((data: Passenger) => {
         this.passenger = data;
       })
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, event);
+      });
   }
 }
